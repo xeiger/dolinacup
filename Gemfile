48b8d9590a82b1e58dc343e7862f457ca9f5166f
Honza Schwarz <cerny.jenda@gmail.com>
2016-05-20 23:22:34 +0200

    zaklad aplikace

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.2.4'
+gem 'pg'
+
+gem 'twitter-bootstrap-rails'
+gem 'jquery-rails'
+
+
+group :production do
+  gem 'rails_12factor'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dolinacup::Application.config.secret_token = '3f70ca328c6b1fb061c921cb9c0312394d41c973eb1472c0daa202c53ddaf6fc36a8e9647cc626ed3ab4a8d0abf6a03f3afadd6219dddf461fdc3f2b365ef6a3'
